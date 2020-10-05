b4ef57c01be69127ecc8d58b922ff1cd54949dbd
BastG57 <bastieng57@gmail.com>
2020-10-05 15:08:51 +0200

    Button + ButtonName + function click button

--- TP1/app/src/main/java/com/tps/tp1app/MainActivity.kt
+++ TP1/app/src/main/java/com/tps/tp1app/MainActivity.kt
@@ -2,11 +2,14 @@ package com.tps.tp1app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // dfhojfdjhsjfvdgchvbn
+    }
+    fun buttonHelloWorldClick(view: View) {
+        // Do something in response to button
     }
 }

256e0b80c1a2b8f22ad90d931fd5c007c3519bea
mouhamed diagne <djira04@gmail.com>
2020-10-05 16:23:16 +0200

    Hello Word app

---
+++ TP1/HelloWord/app/src/main/java/com/nsallmdiagn01/helloword/HelloWordActivity.java
@@ -0,0 +1,14 @@
+package com.nsallmdiagn01.helloword;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HelloWordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_word);
+    }
+}

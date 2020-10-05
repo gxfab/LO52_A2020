dd841a52c69e5dceec8f8ddfb34e7982737acfc3
kAgotsi <cyrilleagotsi@gmail.com>
2020-10-05 15:01:22 +0200

    Add navigate to second activity method.

---
+++ TP1/sources/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.helloworld;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void navigate(View view) {
+        Intent intent = new Intent(MainActivity.this, HelloActivity.class);
+        startActivity(intent);
+    }
+
+}

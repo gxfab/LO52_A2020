8c9d2149181734fb716cb28dc53c34cf87fcef49
BastG57 <bastieng57@gmail.com>
2021-01-02 17:53:12 +0100

    "Update TP4"

--- TP4/AppTP4/app/src/main/cpp/native-lib.cpp
+++ TP4/AppTP4/app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,6 @@
 #include <jni.h>
 #include <string.h>
+#include <iostream>  //For stoi()
 
 extern "C" JNIEXPORT jstring JNICALL   //Récupère le bouton en paramètre et renvoi une string avec la direction correspondante
 Java_com_tps_apptp4_MainActivity_callDirection(JNIEnv *env, jobject obj, jstring myButtonName)
@@ -11,4 +12,47 @@ Java_com_tps_apptp4_MainActivity_callDirection(JNIEnv *env, jobject obj, jstring
     if (strcmp(nativeString, "LEFT") == 0) return env->NewStringUTF("左");
     if (strcmp(nativeString, "RIGHT") == 0) return env->NewStringUTF("右");
     env->ReleaseStringUTFChars(myButtonName, nativeString);
+}
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_tps_apptp4_MainActivity_callMultiplication(JNIEnv *env, jobject thiz, jint my_number, jstring my_button_name)
+{
+    //const char* nativeString2 = env->GetIntField(my_number, JNI_FALSE);
+    if (my_number > 0 && my_number < 11)
+    {
+        //Récupère le button name et le converti en native string
+        const char* nativeString = env->GetStringUTFChars(my_button_name, JNI_FALSE);
+
+        //String qui contiendra la string finale à retourner
+        char resultString[500];
+
+        //Check button name value
+        if  (strcmp(nativeString, "READ") == 0)
+        {
+            int calcul = my_number * my_number;
+            char numberToString[100];
+            sprintf(numberToString, "%d", calcul); //Cast le resultat du calcul dans un char[]
+
+            strcpy(resultString,"READ : ");        //Copy string "READ :" into the result.
+            strcat(resultString, numberToString);       //Puis concatène le résultat du calcul
+
+            return env->NewStringUTF(resultString);     //Auto conversion to const char* par la JNI lors du retour
+        }
+        else if (strcmp(nativeString, "WRITE") == 0)
+        {
+            int calcul = my_number * my_number * my_number;
+            char numberToString[100];
+            sprintf(numberToString, "%d", calcul);
+
+            strcpy(resultString,"WRITE : ");
+            strcat(resultString, numberToString);
+
+            return env->NewStringUTF(resultString);
+        }
+    }
+    else //Le nombre sort du range
+    {
+        return env->NewStringUTF("Error : Type a number between 1 and 10");
+    }
+
 }
--- TP4/AppTP4/app/src/main/java/com/tps/apptp4/MainActivity.kt
+++ TP4/AppTP4/app/src/main/java/com/tps/apptp4/MainActivity.kt
@@ -2,7 +2,6 @@ package com.tps.apptp4
 
 
 import android.os.Bundle
-import android.text.Editable
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -11,28 +10,39 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main) //Recupère les views du layout
 
-        /***************Listeners pour le click des boutons UP/DOWN/LEFT/RIGHT***********************/
+        /***************Listeners pour le click des boutons UP/DOWN/LEFT/RIGHT et READ/WRITE***********************/
         buttonUp.setOnClickListener {
             var direction: String = callDirection("UP")  //Lance la fonction C++ et récupère le résultat
-            textviewDef?.setText(direction)
+            textViewDirection.text = direction
         }
         buttonDown.setOnClickListener {
             var direction: String = callDirection("DOWN")
-            textviewDef?.setText(direction)
+            textViewDirection.text = direction
         }
         buttonRight.setOnClickListener {
             var direction: String = callDirection("RIGHT")
-            textviewDef?.setText(direction)
+            textViewDirection.text = direction
         }
         buttonLeft.setOnClickListener {
             var direction: String = callDirection("LEFT")
-            textviewDef?.setText(direction)
+            textViewDirection.text = direction
+        }
+        buttonRead.setOnClickListener {
+            var number: Int = Integer.parseInt(textViewNumber.text.toString()); //Convertion Editable to Int
+            var read: String = callMultiplication(number, "READ")
+            textViewDirection.text = read
+        }
+        buttonWrite.setOnClickListener {
+            var number: Int = Integer.parseInt(textViewNumber.text.toString());
+            var write: String = callMultiplication(number, "WRITE")
+            textViewDirection.text = write
         }
 
     }
-    external fun callDirection(myButtonName: String) : String     //Declaration fonction C++
+    external fun callDirection(myButtonName: String) : String     //Declaration fonctions C++
+    external fun callMultiplication(myNumber: Int, myButtonName: String) : String
 
     companion object {
         // Used to load the 'native-lib' library on application startup.

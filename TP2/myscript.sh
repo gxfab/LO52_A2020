efea414489c397a83c333874de0ba2ef905d3ca5
lunatic-Satellite-Pro-R50-C <meriadeg.guichard@utbm.fr>
2021-01-05 16:28:24 +0100

    modification succinctes

--- TP2/myscript.sh
+++ TP2/myscript.sh
@@ -4,7 +4,7 @@
 mkdir ~/android-kernel && cd ~/android-kernel
 
 # On fait l'initialisation en prenant la branche hikey-linaro-4.19
-repo init -u https://android.googlesource.com/kernel/manifest -b hikey-linaro-android-4.19
+repo init -u https://android.googlesource.com/kernel/manifest -b hikey-linaro-android-4.19 --repo-url ~/Téléchargements/clone.bundle
 
 # Utilisation de repo syc (on télécharge les fichiers sources)
 repo sync
@@ -15,9 +15,6 @@ export ARCH=arm64
 # Positionnement dans le dossier hikey-linaro qui contient le Makefile
 cd hikey-linaro
 
-# On copie la config de la carte ranchu dans le répertoire configs
-cp arch/mips/configs/generic/board-ranchu.config arch/arm64/configs/
-
 # On nettoie avec les commandes make clean et make mrproper
 make clean
 make mrproper
@@ -25,6 +22,9 @@ make mrproper
 # On charge la configuration de base pour hikey puis on la sauvegarde dans un répertoire configs
 make hikey_defconfig
 cp .config configs/hikey_defconfig
+
+# On copie la config de la carte ranchu dans le répertoire configs
+cp arch/mips/configs/generic/board-ranchu.config arch/arm64/configs/
 
 # On charge la configuration de la carte ranchu
 make oldconfig board-ranchu.config
